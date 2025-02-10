befd8afd2682aad0033248928f3f5f42ffdca3a2
parch-ns <parch.k@ku.th>
2025-02-10 16:43:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,74 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const host = 'localhost';
-const port = 8000
+app.use(bodyParser.json());
+const port = 8000;
 
-const requestListener = function (req,res){
-    res.writeHead(200)
-    res.end('My first server!')
-}
+//เก็บuser
+let users = []
+let counter = 1
 
-const server = http.createServer(requestListener)
-    server.listen(port, host , () => {
-        console.log(`Server is running on http://${host}:${port}`)
-    })
+/*
+Get /users สำหรับดึงข้อมูล user ทั้งหมด
+*/
+
+// path = GET /users
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+// path = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1
+    users.push(user);
+    res.json({
+        message: "User created",
+        user: user
+    });
+})
+
+//path = PUT /user/:id
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+    //หาuserจากidที่ส่งมา
+    let selectedIndex = users.findIndex(user =>user.id == id )
+    //update user
+    if(updateUser.firstname){
+        users[selectedIndex].firstname = updateUser.firstname
+    }
+
+    if(updateUser.lastname){
+        users[selectedIndex].lastname = updateUser.lastname
+    }
+
+    res.json({
+        message: "User updated",
+        data :{
+            user: updateUser,
+            indexUpdate: selectedIndex
+        }
+    });
+    //ส่งข้อมูล user ที่update กลับเข้าที่เดิม
+    
+})
+
+//path = DELETE /user/:id
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    //หา index ของ user ที่ต้องการลบ
+    let selectedIndex = users.findIndex(user => user.id == id)
+    
+    users.splice(selectedIndex, 1)
+    res.json({
+        message: "Delete Completed",
+        indexedDeleted: selectedIndex
+    });
+})
+
+app.listen(port, (req, res) => {
+    console.log('Server is running on port' + port);
+});
